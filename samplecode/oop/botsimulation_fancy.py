68df4674aebfe8108fc26ddb260ee9beef355469
David Dumas <david@dumas.io>
2024-01-22 14:08:40 -0600

    Revise and comment fancy simulation bot list

--- samplecode/oop/botsimulation_fancy.py
+++ samplecode/oop/botsimulation_fancy.py
@@ -15,22 +15,24 @@ width = 60
 height = 30
 
 current_bots = []
+# 5 static bots
 for _ in range(5):
     P = Point2(random.randint(0, width - 1), random.randint(0, height - 1))
     current_bots.append(bots.Bot(position=P))
+# 5 wandering bots (N,S,E,W)
 for _ in range(5):
     P = Point2(random.randint(0, width - 1), random.randint(0, height - 1))
     current_bots.append(bots.WanderBot(position=P))
+# 5 fast-wandering bots (N,S,E,W,NE,NW,SE,SW)
 for _ in range(5):
     P = Point2(random.randint(0, width - 1), random.randint(0, height - 1))
     current_bots.append(bots.FastWanderBot(position=P))
+# 5 self-destructing bots (20 steps before they're gone)
 for _ in range(5):
     P = Point2(random.randint(0, width - 1), random.randint(0, height - 1))
     current_bots.append(bots.DestructBot(position=P, active_time=20))
-for _ in range(5):
-    P = Point2(random.randint(0, width - 1), random.randint(0, height - 1))
-    current_bots.append(bots.FastWanderBot(position=P))
 
+# Diagonal patrol bot
 current_bots.append(
     bots.PatrolBot(
         position=Point2(2, 2),
@@ -39,21 +41,13 @@ current_bots.append(
     )
 )
 
+# Horizontal patrol bot
 current_bots.append(
     bots.PatrolBot(
         position=Point2(58, 5),
         direction=Vector2(-1, 0),
         steps=27,
     )
-)
-
-
-current_bots.append(
-    bots.PatrolBot(position=Point2(5, 5), direction=Vector2(1, 1), steps=4)
-)
-
-current_bots.append(
-    bots.PatrolBot(position=Point2(10, 2), direction=Vector2(-1, 0), steps=7)
 )
 
 no_symbol_bot_types = []
