5a417fcd1c5e54348973b2cdc9ad90d4f6077763
David Dumas <david@dumas.io>
2024-01-29 14:33:27 -0600

    Live coding 1pm lecture 9

--- samplecode/recursion/recexamp.py
+++ samplecode/recursion/recexamp.py
@@ -1,4 +1,4 @@
-# MCS 275 Spring 2024 Lecture 8
+# MCS 275 Spring 2024 Lectures 8 and 9
 # Recursion examples
 # David Dumas
 
@@ -18,6 +18,12 @@ def fac(n, verbose=False):
     # recursive call
     return n * fac(n - 1, verbose)
 
+def fac_iterative(n):
+    "compute factorial with iteration"
+    prod = 1 # STATE
+    for i in range(2,n+1): # start i at 1, end with i=n
+        prod *= i
+    return prod
 
 # WARNING: For large n, this is ridiculously inefficient.
 # More discussion in lecture 9.
@@ -34,6 +40,14 @@ def fib(n, verbose=False):
     return fib(n - 1, verbose) + fib(n - 2, verbose)
     # NOTE: ^--------------------^--- two self-calls = SLOW
 
+def fib_iterative(n):
+    "iterative fibonacci"
+    # a,b will be the last two Fib terms we know
+    a = 0  #F_0
+    b = 1  #F_1
+    for i in range(n):
+        a, b = b, a+b # BTW this works
+    return a
 
 # Paper-folding sequence
 # ----------------------
@@ -57,3 +71,38 @@ def PFS(n):
     L = PFS(n - 1)
     return L + [1] + [1 - x for x in L[::-1]]
     #                   ^-- flip         ^-- reverse
+
+
+def timing_study():
+    for n in range(0,995,50):
+        t0 = time.time()
+        y = fac(n)
+        t1 = time.time()
+        trec = t1-t0  # amount of time it took to compute fac(n)
+
+        t0 = time.time()
+        y = fac_iterative(n)
+        t1 = time.time()
+        titer = t1-t0  # amount of time it took to compute fac_iterative(n)
+
+        print("fac({:3d}): rec took {:.4f}s, iter took {:.4f}s".format(n,trec,titer))
+
+    print("\n\n")
+    for n in range(36):
+        t0 = time.time()
+        y = fib(n)
+        t1 = time.time()
+        trec = t1-t0  # amount of time it took to compute fac(n)
+
+        t0 = time.time()
+        y = fib_iterative(n)
+        t1 = time.time()
+        titer = t1-t0  # amount of time it took to compute fac_iterative(n)
+
+        print("fib({:3d}): rec took {:.4f}s, iter took {:.4f}s".format(n,trec,titer))
+
+
+if __name__ == "__main__":
+    import time
+    # we're run as a script, so do stuff (not just function defs)
+    timing_study()
