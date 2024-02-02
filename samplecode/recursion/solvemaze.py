fd044059fffd98a61b4a5278aa80ed9482275061
David Dumas <david@dumas.io>
2024-02-02 14:54:02 -0600

    Change default depth_first_maze_solution to allow loops
    There is now a second function `depth_first_maze_unique_solution`
    that implements the faster method from Lecture 11 but will not work
    for mazes that have loops.
    
    Supporting mazes with loops is a good idea since Worksheet 5 will
    involve creating some custom mazes, and it's easier to make mazes
    with loops than without them.  Thus this change should make the default
    behavior of the solver more intuitive.

--- samplecode/recursion/solvemaze.py
+++ samplecode/recursion/solvemaze.py
@@ -4,12 +4,51 @@
 
 import maze
 
+# Note: mazes with few walls tend to have loops
+# which create problems for the algorithm in
+# lecture 11. Since maze.py supports building
+# mazes from scratch by adding walls, it seems
+# best to have the maze solver allow loops by
+# default.  So this module offers both.
+
 
 def depth_first_maze_solution(M, path=None):
     """
     Take a Maze object `M` and optional partial
     progress `path` (list of Point2 objects) and
     complete to a solution recursively.
+    **This function can solve mazes with loops.**
+    """
+    if path == None:
+        # no path given, begin at M.start
+        path = [M.start]
+    if path[-1] == M.goal:
+        # path is a solution, return!
+        return path
+
+    # enumerate possible next steps
+    possible_next = M.free_neighbors(path[-1])
+    # then remove any places we've already been
+    possible_next = [x for x in possible_next if x not in path]
+
+    # consider next steps
+    for next_step in possible_next:
+        new_path = path + [next_step]
+        res = depth_first_maze_solution(M, new_path)
+        if res:  # if res is a nonempty list, i.e. not None
+            # res is in fact a solution; return it
+            return res
+
+    # no possible next step worked out
+    return None
+
+
+def depth_first_maze_unique_solution(M, path=None):
+    """
+    Take a Maze object `M` and optional partial
+    progress `path` (list of Point2 objects) and
+    complete to a solution recursively.
+    **This function CANNOT solve mazes with loops.**
     """
     if path == None:
         # no path given, begin at M.start
@@ -29,7 +68,7 @@ def depth_first_maze_solution(M, path=None):
     # consider next steps
     for next_step in possible_next:
         new_path = path + [next_step]
-        res = depth_first_maze_solution(M, new_path)
+        res = depth_first_maze_unique_solution(M, new_path)
         if res:  # if res is a nonempty list, i.e. not None
             # res is in fact a solution; return it
             return res
