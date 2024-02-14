27801eab6adef0cc0e5366545d0d72e3da42bace
David Dumas <david@dumas.io>
2024-02-14 13:01:04 -0600

    12pm live coding

--- samplecode/datastructures/trees.py
+++ samplecode/datastructures/trees.py
@@ -30,3 +30,33 @@ class Node:
     def __repr__(self):
         "Unambiguous string representing this object, intended for developers"
         return str(self)
+
+class BST(Node):
+    "Binary search tree supporting search by key and node insertion"
+    def search(self,k):
+        "Find and return a node with key `k`, or None if no such node exists"
+        if k == self.key:
+            # This node is the right one, return it
+            return self
+        # k is not the key of self, so if k is present it's in the left or right subtree
+
+        if k < self.key:
+            if self.left != None:
+                # the left subtree exists, so search it
+                return self.left.search(k)
+            else:
+                # k would need to be in the left subtree
+                # but there's no left subtree!
+                return None # not found
+        else:
+            if self.right != None:
+                return self.right.search(k)
+            else:
+                return None
+            
+    def insert(self,k):
+        """
+        Create a node with key `k` and add it to the tree, preserving the BST property
+        Requires that key `k` is not present in the tree already!
+        """
+        
