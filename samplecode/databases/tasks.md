94c98f4e33c0c238d84bc30cc083fa8f31b0a597
David Dumas <david@dumas.io>
2024-03-13 12:51:46 -0500

    12pm tsk list example

---
+++ samplecode/databases/tasks.md
@@ -0,0 +1,34 @@
+# Tasks application
+
+## Add a task
+
+e.g.
+```
+python3 tasks.py add "Fix insert() method in project 3"
+```
+
+## List tasks
+
+Print a list of tasks, labeling each one with its task id.
+
+```
+python3 tasks.py list
+```
+
+### List all (i.e. include completed tasks)
+
+```
+python3 tasks.py list all
+```
+
+## Mark task as completed
+
+```
+python3 tasks.py done [TASKID]
+```
+
+## Delete a task
+
+```
+python3 tasks.py delete [TASKID]
+```
---
+++ samplecode/databases/tasks.py
@@ -0,0 +1,99 @@
+# MCS 275 Spring 2024 Lecture 28
+# David Dumas
+"Task list manager using SQLite for storage"
+
+import sqlite3
+import sys
+
+
+def get_db_connection():
+    con = sqlite3.connect("tasks.db")
+    # any verification or initialization can go here
+    return con
+
+
+def do_add(description):
+    con = get_db_connection()
+    con.execute(
+        """
+        INSERT INTO tasks (desc)
+        VALUES (?);
+                """,
+        [description],
+    )
+    con.commit()
+    con.close()
+
+
+def do_list(flag):
+    con = get_db_connection()
+    if flag == None:
+        res = con.execute(
+            """
+            SELECT id,desc,complete FROM tasks WHERE complete=0;
+                    """
+        )
+    elif flag == "all":
+        res = con.execute(
+            """
+            SELECT id,desc,complete FROM tasks;
+                    """
+        )
+    else:
+        raise ValueError("Unknown flag '{}'".format(flag))
+    for taskid, desc, complete in res:
+        if complete:
+            donechar = "\u2713"
+        else:
+            donechar = " "
+        print("{}{:3d} {}".format(donechar, taskid, desc))
+    con.close()
+
+
+def do_done(taskid_str):
+    taskid = int(taskid_str)
+    con = get_db_connection()
+    con.execute(
+        """
+        UPDATE tasks SET complete=1 WHERE id=?;
+                """,
+        [taskid],
+    )
+    con.commit()
+    con.close()
+
+
+def do_delete(taskid_str):
+    taskid = int(taskid_str)
+    con = get_db_connection()
+    res = con.execute(
+        """
+        DELETE FROM tasks WHERE id=?;
+                """,
+        [taskid],
+    )
+    if res.rowcount == 0:
+        print("WARNING: No such task")
+    con.commit()
+    con.close()
+
+
+if __name__ == "__main__":
+    # sys.argv[0] is the name of this script
+    # sys.argv[1] is the first command line argument
+    # ...
+    command = sys.argv[1]  # e.g. "add", "list", "delete"
+    operand = None
+    if len(sys.argv) > 2:
+        operand = sys.argv[2]  # data used by the command, e.g. task description
+
+    if command == "add":
+        do_add(operand)
+    elif command == "list":
+        do_list(operand)
+    elif command == "done":
+        do_done(operand)
+    elif command == "delete":
+        do_delete(operand)
+    else:
+        raise ValueError("I don't know how to do that.")
